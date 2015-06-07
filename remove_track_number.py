4fe748862abb6e0a684b3a650bc23fd42f1d3d18
tracy rohlin <tracy.rohlin@gmail.com>
2015-06-07 11:24:57 -0700

    Program now forces capitalization after any parentheses and also removes featuring artists from song title.

--- remove_track_number.py
+++ remove_track_number.py
@@ -3,14 +3,36 @@ __author__ = 'tracyrohlin'
 
 import os, shutil, sys
 from string import capwords
-from re import match
+import re
 
 def remove_numbers(root, name, file_tag):
-    if match(r"\d\d", name[0:2]):
-        new_name = capwords((name.split(file_tag))[0][3:]) + file_tag
+    print "I am in program"
+    if re.match(r"\d\d", name[0:2]):
+        new_name = capwords((name.split(file_tag))[0][3:])
+
+        # gets rid of (featuring artist) and (ft. artist) from file name
+        feat_pattern = re.compile(r'.feat.+')
+        ft_pattern = re.compile(r'(ft.+)')
+        patterns = [feat_pattern, ft_pattern]
+        for pattern in patterns:
+            match = re.search(pattern, new_name)
+            if match:
+                new_name = pattern.sub("", new_name)[:-1]
+            else:
+                pass
+
+        # forces capitalization after a parentheses
+        i=0
+        pattern = re.compile(r"\(")
+        for p_match in re.finditer(pattern, new_name):
+            i = p_match.start()+1
+        new_name = new_name[:i]+ new_name[i:].capitalize()
+        new_name+= file_tag
+
         new_file_path = os.path.join(root, new_name)
         old_file_path = root + "/" + name
         shutil.move(old_file_path, new_file_path)
+
 
 
 def main(file_path):
@@ -27,8 +49,8 @@ def main(file_path):
                 elif name.endswith("flac"):
                     remove_numbers(root, name, ".flac")
                 else: pass
-            except:
-                pass
+            except Exception as error:
+                print error
 
     print "I am done"
 
